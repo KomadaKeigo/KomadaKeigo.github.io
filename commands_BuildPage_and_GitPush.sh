bdd4e10c900a870e9a20d017fc4e9ed5efbbdb9b
KomadaKeig <keigo.komada.r6@dc.tohoku.ac.jp>
2023-03-07 12:00:02 +0900

    Push用のシェルスクリプトを書いた

---
+++ commands_BuildPage_and_GitPush.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#よく分からんけど効かない...qsub以外では無効??→  #$ -cwd
+
+cd "$(dirname "$0")"
+
+echo "Enter Git Commit Comment!"
+read COMMENT_RAW
+#echo "Raw Comment = $COMMENT_RAW"
+
+#Delete Spaces from Input
+#tr -d string stringみたいな使い方はできないんすねー
+COMMENT_SPACES_TRIMMED=`echo $COMMENT_RAW | tr -d ' 　'`
+
+if [ -z "$COMMENT_SPACES_TRIMMED" ]; then
+    echo "It's Empty Comment"
+    TIME_STAMP=`date "+%Y/%m/%d %H:%M:%S"`
+    COMMENT=`echo "commit at $TIME_STAMP"`
+else
+    COMMENT=$COMMENT_RAW
+fi
+echo "COMMENT = $COMMENT"
+
+#build page
+hugo
+#Show Address
+echo "↓Your Page's URL"
+grep "baseURL = \"*\"" config.toml | grep -o "https://.*\.github\.io/"
+
+
+#do git push
+git add .
+git commit -m "$COMMENT"
+git push
+
+
